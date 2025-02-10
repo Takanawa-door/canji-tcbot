f6452c960c325a8c58927121e3a23b7f253bdb63
Takanawa-door <1007929413@qq.com>
2025-02-10 22:04:29 +0800

    修正 setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="",
-    version="",
+    name="tcbot",
+    version="1.0.0",
     packages=find_packages(),
     install_requires = [
         "selenium>=4.28.1",
